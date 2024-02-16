d2ea87aaf69d1e7904f4c36be109fa08f47a6f20
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-02-15 19:28:40 -0500

    added more missing syntax/removed other sequelize function

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const express = require('express');
-const router = require('express').Router()
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers')
+const sequelize = require('./config/connection');
+const helpers = require('./utils');
+
 const app = express();
 const PORT = process.env.PORT || 3306;
-const sequelize = require('./config/connection');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -19,12 +28,11 @@ const sess = {
   }),
 };
 
-// var sequelize = new Sequelize("database", "username", "password", {
-//   dialect: "mysql",
-//   storage: "./session.sqlite",
-// });
 
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -33,7 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers/'));
+app.use(require(routes));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
